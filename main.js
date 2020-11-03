e1891d888de6729d66e8fb75cf8c572654df3a06
Alexpy51 <73444730+Alexpy51@users.noreply.github.com>
2020-11-03 13:28:31 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,5 @@
+
+document.getElementById("demo").innerHTML = "JavaScript has worked."
+
+if (document.getElementById("demo").innerHTML == "JavaScript has worked.")
+    window.alert("This is also Javascript.")
